c07af7b1dc9247a82b288e8718b9f675872f8429
INFOPULSE\Anton.Kulish <Anton.Kulish@infopulse.com>
2017-11-17 13:19:59 +0200

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello world!");
+        //valentin
+        //asdasdasd
+        //asdasd
+
     }
 }
